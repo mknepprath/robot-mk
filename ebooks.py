e87b6febb2225d2e0af52e0de1b6eb580d093228
Michael Knepprath <michael@mknepprath.com>
2016-08-27 00:08:14 -0500

    fixes a bunch of linting issues

--- ebooks.py
+++ ebooks.py
@@ -1,11 +1,12 @@
+"""Main file for @robot_mk."""
 import os
 import random
 import re
 import sys
+from htmlentitydefs import name2codepoint as n2c
 import tweepy
 import twitter
 import markov
-from htmlentitydefs import name2codepoint as n2c
 from local_settings import *
 
 class TwitterAPI:
@@ -34,13 +35,15 @@ class TwitterAPI:
         self.api.update_status(status=message, in_reply_to_status_id=tweet_id)
 
 def connect():
+    """Connects to the Twitter API."""
     api = twitter.Api(consumer_key=MY_CONSUMER_KEY,
-                        consumer_secret=MY_CONSUMER_SECRET,
-                        access_token_key=MY_ACCESS_TOKEN_KEY,
-                        access_token_secret=MY_ACCESS_TOKEN_SECRET)
+                      consumer_secret=MY_CONSUMER_SECRET,
+                      access_token_key=MY_ACCESS_TOKEN_KEY,
+                      access_token_secret=MY_ACCESS_TOKEN_SECRET)
     return api
 
 def entity(text):
+    """Checks something, but I'm not sure what."""
     if text[:2] == "&#":
         try:
             if text[:3] == "&#x":
@@ -59,20 +62,33 @@ def entity(text):
     return text
 
 def filter_tweet(tweet):
-    tweet.text = re.sub(r'\b(RT|MT) .+','',tweet.text) #take out anything after RT or MT
-    tweet.text = re.sub(r'(\#|@|(h\/t)|(http))\S+','',tweet.text) #Take out URLs, hashtags, hts, etc.
-    tweet.text = re.sub(r'\n','', tweet.text) #take out new lines.
+    """Cleans up tweets gathered."""
+    tweet.text = re.sub(r'\b(RT|MT) .+', '', tweet.text) #take out anything after RT or MT
+    tweet.text = re.sub(
+        r'(\#|@|(h\/t)|(http))\S+',
+        '',
+        tweet.text
+    ) #Take out URLs, hashtags, hts, etc.
+    tweet.text = re.sub(r'\n', '', tweet.text) #take out new lines.
     tweet.text = re.sub(r'\"|\(|\)', '', tweet.text) #take out quotes.
     htmlsents = re.findall(r'&\w+;', tweet.text)
-    if len(htmlsents) > 0 :
+    if len(htmlsents) > 0:
         for item in htmlsents:
             tweet.text = re.sub(item, entity(item), tweet.text)
     tweet.text = re.sub(r'\xe9', 'e', tweet.text) #take out accented e
     return tweet.text
 
 def grab_tweets(api, max_id=None):
-    source_tweets=[]
-    user_tweets = api.GetUserTimeline(screen_name=user, count=200, max_id=max_id, include_rts=True, trim_user=True, exclude_replies=True)
+    """Gets tweets from @robot_mk."""
+    source_tweets = []
+    user_tweets = api.GetUserTimeline(
+        screen_name=user,
+        count=200,
+        max_id=max_id,
+        include_rts=True,
+        trim_user=True,
+        exclude_replies=True
+    )
     max_id = user_tweets[len(user_tweets)-1].id-1
     for tweet in user_tweets:
         tweet.text = filter_tweet(tweet)
@@ -80,15 +96,15 @@ def grab_tweets(api, max_id=None):
             source_tweets.append(tweet.text)
     return source_tweets, max_id
 
-if __name__=="__main__":
+if __name__ == "__main__":
     order = ORDER
-    if DEBUG==False:
+    if not DEBUG:
         guess = random.choice(range(ODDS))
     else:
         guess = 0
 
     if guess == 0:
-        if STATIC_TEST==True:
+        if STATIC_TEST:
             file = TEST_SOURCE
             print ">>> Generating from {0}".format(file)
             string_list = open(file).readlines()
@@ -98,11 +114,11 @@ if __name__=="__main__":
             #gets tweets
             source_tweets = []
             for handle in SOURCE_ACCOUNTS:
-                user=handle
-                api=connect()
-                max_id=None
+                user = handle
+                api = connect()
+                max_id = None
                 for x in range(17)[1:]:
-                    source_tweets_iter, max_id = grab_tweets(api,max_id)
+                    source_tweets_iter, max_id = grab_tweets(api, max_id)
                     source_tweets += source_tweets_iter
                 print "{0} tweets found in {1}".format(len(source_tweets), handle)
                 if len(source_tweets) == 0:
@@ -113,21 +129,21 @@ if __name__=="__main__":
             if re.search('([\.\!\?\"\']$)', tweet):
                 pass
             else:
-                tweet+="."
+                tweet += "."
             mine.add_text(tweet)
 
-        for x in range(0,10):
+        for x in range(0, 10):
             ebook_tweet = mine.generate_sentence()
 
         #randomly drop the last word, as Horse_ebooks appears to do.
-        if random.randint(0,4) == 0 and re.search(r'(in|to|from|for|with|by|our|of|your|around|under|beyond)\s\w+$', ebook_tweet) != None:
-           print "Losing last word randomly"
-           ebook_tweet = re.sub(r'\s\w+.$','',ebook_tweet)
-           print ebook_tweet
+        if random.randint(0, 4) == 0 and re.search(r'(in|to|from|for|with|by|our|of|your|around|under|beyond)\s\w+$', ebook_tweet) != None:
+            print "Losing last word randomly"
+            ebook_tweet = re.sub(r'\s\w+.$', '', ebook_tweet)
+            print ebook_tweet
 
         #if a tweet is very short, this will randomly add a second sentence to it.
         if ebook_tweet != None and len(ebook_tweet) < 40:
-            rando = random.randint(0,10)
+            rando = random.randint(0, 10)
             if rando == 0 or rando == 7:
                 print "Short tweet. Adding another sentence randomly"
                 newer_tweet = mine.generate_sentence()
@@ -155,13 +171,13 @@ if __name__=="__main__":
                 print "DRIBBBLE TWEET: " + ebook_tweet
                 sys.exit()
 
-            if DEBUG == False:
+            if not DEBUG:
                 status = api.PostUpdate(ebook_tweet)
                 print status.text.encode('utf-8')
             else:
                 print ebook_tweet
 
-        elif ebook_tweet == None:
+        elif ebook_tweet is None:
             print "Tweet is empty, sorry."
         else:
             print "TOO LONG: " + ebook_tweet
