5c5bca2adda46485a10384e6c680105c1ed4e5fe
mknepprath <mknepprath@gmail.com>
2017-03-30 18:30:13 -0500

    Increase frequency of tweet attempts (in Heroku), decrease odds of being able to tweet

--- local_settings.py
+++ local_settings.py
@@ -3,10 +3,10 @@ Local Settings for robot_mk.
 '''
 
 SOURCE_ACCOUNTS = ["mknepprath"] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
-ODDS = 2 #How often do you want this to run? 1/8 times?
-FAVE_ODDS = 8
-REPLY_ODDS = 4
-QUOTE_ODDS = 16
+ODDS = 18 #How often do you want this to run? 1/8 times?
+FAVE_ODDS = 72
+REPLY_ODDS = 36
+QUOTE_ODDS = 144
 ORDER = 2 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
 DEBUG = False #Set this to False to start Tweeting live
 TWEET_ACCOUNT = "robot_mk"
