648600a4858b026e2d6faed3e958bd0c7ddb1b1d
Michael Knepprath <mknepprath@gmail.com>
2021-07-02 23:26:28 -0400

    Switch to openai

--- ebooks.py
+++ ebooks.py
@@ -1,12 +1,15 @@
 import os
+import openai
 import random
 import re
 import sys
-from htmlentitydefs import name2codepoint
 from datetime import datetime
 import tweepy
-import markov
 from local_settings import *
+
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+delimiter = "\n---\n"
 
 
 class TwitterAPI:
@@ -74,7 +77,7 @@ def get_tweets(twitter, screen_name, max_id=None):
     )
 
     # Gets the ID of the last tweet returned.
-    max_id = user_tweets[len(user_tweets)-1].id-1
+    max_id = user_tweets[len(user_tweets)-1].id - 1
 
     # Loops through all returned tweets.
     for tweet in user_tweets:
@@ -105,96 +108,70 @@ if __name__ == '__main__':
 
     # Checks the current time before tweeting. This bot sleps.
     current_hour = datetime.now().hour
-    awake = current_hour <= 3 or current_hour >= 11
-    print 'I\'m awake. TWEET O\'CLOCK!' if awake else 'slepin.'
+    awake = True
+    if awake:
+        print("I'm awake.")
+    else:
+        print("I'm asleep.")
 
     # Instantiates empty tweets list.
     source_tweets = []
     source_replies = []
 
     if guess == 0 and awake:
-        print 'Fetching tweets...'
+        print('Fetching tweets...')
         # Populates tweets list.
         for screen_name in SOURCE_ACCOUNTS:
             # Reset max_id for each account.
             max_id = None
 
             # Gets a bunch of tweets.
-            for x in range(17)[1:]:
+            for x in range(4)[1:]:
                 source_tweets_iter, source_replies_iter, max_id = get_tweets(
                     twitter, screen_name, max_id)
                 source_tweets += source_tweets_iter
                 source_replies += source_replies_iter
-            print '{0} tweets and {1} replies found in @{2}.'.format(len(source_tweets), len(source_replies), screen_name)
+            print('{0} tweets and {1} replies found in @{2}.'.format(
+                len(source_tweets), len(source_replies), screen_name))
+
             if len(source_tweets) == 0:
-                print 'Error fetching tweets from Twitter. Aborting.'
+                print('Error fetching tweets from Twitter. Aborting.')
                 sys.exit()
 
     if guess == 0 and awake:
-        print 'Generating tweets...'
+        print('\nGenerating tweets...')
 
-        mine = markov.MarkovChainer()
-
-        # If the tweet doesn't end with punctuation, add a period.
-        for tweet in source_tweets:
-            if re.search('([\.\!\?\"\']$)', tweet):
-                pass
-            else:
-                tweet += '.'
-            mine.add_text(tweet)
-
-        for x in range(0, 10):
-            ebook_tweet = mine.generate_sentence()
-
-        # randomly drop the last word, as Horse_ebooks appears to do.
-        if random.randint(0, 4) == 0 and re.search(r'(in|to|from|for|with|by|our|of|your|around|under|beyond)\s\w+$', ebook_tweet) != None:
-            print 'Losing last word randomly.'
-            ebook_tweet = re.sub(r'\s\w+.$', '', ebook_tweet)
-            print ebook_tweet
-
-        # If a tweet is very short, this will randomly add a second sentence to it.
-        if ebook_tweet != None and len(ebook_tweet) < 40:
-            # Get random number.
-            random_int = random.randint(0, 10)
-
-            # 1/5 chance of adding another tweet to tweet.
-            if random_int < 3:
-                print 'Short tweet. Randomly adding another sentence.'
-                next_ebook_tweet = mine.generate_sentence()
-
-                # If the new tweet has text, add the text.
-                if next_ebook_tweet != None:
-                    ebook_tweet += ' ' + next_ebook_tweet
-
-            # 1/10 chance of uppercasing tweet.
-            elif random_int == 2:
-                # say something crazy/prophetic in all caps.
-                print 'ALL THE THINGS'
-                ebook_tweet = ebook_tweet.upper()
+        response = openai.Completion.create(
+            engine="davinci", prompt=delimiter.join(source_tweets[:80]) + "." + delimiter, max_tokens=40)
+        print('OpenAI candidates:')
+        print(response.choices[0].text)
+        openai_tweet = response.choices[0].text.split(delimiter)[0].strip()
 
         # throw out tweets that match anything from the source account.
-        if ebook_tweet != None and len(ebook_tweet) < 240:
+        if openai_tweet != None and len(openai_tweet) < 240:
             for tweet in source_tweets:
-                if ebook_tweet[:-1] not in tweet:
+                if openai_tweet[:-1] not in tweet:
                     continue
                 else:
-                    print 'TOO SIMILAR: ' + ebook_tweet
+                    print('TOO SIMILAR: ' + openai_tweet)
                     sys.exit()
 
             if not DEBUG:
-                twitter.tweet(ebook_tweet)
+                twitter.tweet(openai_tweet)
+                print('Tweeted \'' + openai_tweet + '\'.')
+            else:
+                print('Didn\'t tweet \'' + openai_tweet +
+                      '\' because DEBUG is True.')
 
-            print 'Tweeted \'' + ebook_tweet + '\'.'
-
-        elif ebook_tweet is None:
-            print 'Tweet is empty, sorry.'
+        elif openai_tweet is None:
+            print('Tweet is empty, sorry.')
         else:
-            print 'TOO LONG: ' + ebook_tweet
+            print('TOO LONG: ' + openai_tweet)
 
         # Let's do stuff with mentions. First, let's get a couple.
         source_mentions = twitter.api.mentions_timeline(count=2)
 
-        print 'Getting last two mentions.'
+        print('\nGetting last two mentions.')
 
         # Get tweets from this bot.
         source_compare_tweets = twitter.api.user_timeline(
@@ -208,52 +185,69 @@ if __name__ == '__main__':
                 # If the mention isn't favorited, favorite it.
                 if not twitter.api.get_status(id=mention.id).favorited:
                     twitter.api.create_favorite(id=mention.id)
-                    print 'Favorited \'' + mention.text + '\''
+                    print('\nFavorited \'' + mention.text + '\'')
 
             # Instantiate replied to false.
             replied = False
 
+            replied_to_tweet = twitter.api.get_status(
+                id=mention.in_reply_to_status_id)
+
+            # Start building prompt.
+            prompt = "robot_mk:" + source_tweets[0] + delimiter + replied_to_tweet.user.screen_name + \
+                ":" + replied_to_tweet.text + delimiter + \
+                mention.user.screen_name + ":" + mention.text + delimiter
+            end = False
+            while end is not False:
+                print(replied_to_tweet.text)
+                if replied_to_tweet.in_reply_to_status_id_str == None:
+                    end = True
+                    break
+                else:
+                    replied_to_tweet = twitter.api.get_status(
+                        id=replied_to_tweet.in_reply_to_status_id)
+                    prompt = replied_to_tweet.user.screen_name + \
+                        ":" + replied_to_tweet.text + delimiter
+                    print(replied_to_tweet.text)
+
+            prompt = prompt + "robot_mk:"
+
+            print("\nPrompt:")
+            print(prompt)
+            print("===\n")
+
             # Check if the current mention matches a tweet this bot has replied to.
             for tweet in source_compare_tweets:
                 if tweet.in_reply_to_status_id == mention.id:
-                    print 'Matches a tweet bot has replied to.'
+                    print('Matches a tweet bot has replied to.')
                     replied = True
 
             # If the bot is awake and has not replied to this mention, reply, sometimes.
             if random.choice(range(REPLY_ODDS)) == 0 and awake and not replied:
-                print 'Generating replies...'
+                print('Generating replies...')
 
-                mine = markov.MarkovChainer()
-
-                for reply in source_replies:
-                    if re.search('([\.\!\?\"\']$)', reply):
-                        pass
-                    else:
-                        reply += '.'
-                    mine.add_text(reply)
-
-                for x in range(0, 10):
-                    ebook_reply = mine.generate_sentence()
-
-                if random.randint(0, 10) == 0:
-                    # Say something crazy/prophetic in all caps.
-                    print 'ALL THE THINGS'
-                    ebook_reply = ebook_reply.upper()
+                response = openai.Completion.create(
+                    engine="davinci", prompt=prompt, max_tokens=40)
+                print('OpenAI candidates:')
+                print(response.choices[0].text)
+                openai_reply = response.choices[0].text.split(delimiter)[
+                    0].strip()
+                print(openai_reply)
 
                 # Throw out tweets that match anything from the source account.
-                if ebook_reply != None and len(ebook_reply) < 240 and not DEBUG:
+                if openai_reply != None and len(openai_reply) < 240 and not DEBUG:
                     # Reply.
                     if random.choice(range(QUOTE_ODDS)) == 0:
-                        ebook_reply += ' http://twitter.com/' + \
+                        openai_reply += ' http://twitter.com/' + \
                             mention.user.screen_name + \
                             '/status/' + str(mention.id)
-                        twitter.tweet(ebook_reply)
-                        print 'Quoted with \'' + ebook_reply + '\''
+                        twitter.tweet(openai_reply)
+                        print('Quoted with \'' + openai_reply + '\'')
                     else:
-                        twitter.reply(ebook_reply, mention.id)
-                        print 'Replied with \'' + ebook_reply + '\''
+                        twitter.reply(openai_reply, mention.id)
+                        print('Replied with \'' + openai_reply + '\'')
             else:
-                print 'Not replying this time.'
+                print('Not replying this time.')
     else:
         # Message if the random number fails.
-        print str(guess) + ' No, sorry, not this time.'
+        print(str(guess) + ' No, sorry, not this time.')
